6c4769799e2f3846983ce0e2595b8e6cbb37f2e7
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2023-10-28 23:54:03 -0300

    Se crea modelo Reservation

--- models/reservation.model.js
+++ models/reservation.model.js
@@ -1,5 +1,20 @@
 const { Schema, model } = require('mongoose');
 
 const reservationSchema = new Schema({
-    
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    date:{
+        type: Date,
+        required: true,
+    },
+    footballField:{
+        type: Schema.Types.ObjectId,
+        ref:'footballField',
+        required: true
+    }
 })
+
+module.exports= new model('Reservation', reservationSchema);
