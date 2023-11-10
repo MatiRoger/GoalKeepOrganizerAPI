dcb5f86d9a17312811babf901da0c1cac458d3c1
Gabriel Mendez <mendez.gabriel.d@hotmail.com>
2023-11-10 13:58:01 -0300

    correcion de peticiones http y update de usuarios

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-const { createUser, getAllUser, login } = require('../services/user.service');
+const { createUser, getAllUser, login, deleteUsersService, updateUsersService } = require('../services/user.service');
 
 const createUsers = async (req, res) => {
     try {
@@ -6,7 +6,7 @@ const createUsers = async (req, res) => {
         res.status(200).json({ newUser });
     } catch (error) {
         console.log(error);
-        res.status(404).json({ error });
+        res.status(400).json({ error: error.message });
     }
 };
 
@@ -17,7 +17,7 @@ const getAllUsers = async (req, res) => {
         res.status(200).json({ allUsers });
     } catch (error) {
         console.log(error);
-        res.status(404).json({ error });
+        res.status(400).json({ error: error.message });
     }
 };
 
@@ -27,12 +27,40 @@ const loginService = async (req, res) => {
         res.status(200).json({ loginUser });
     } catch (error) {
         console.log(error);
-        res.status(404).json({ error });
+        res.status(401).json({ error: error.message });
+    }
+};
+
+const deleteUsers = async (req, res) => {
+    try {
+        const { id } = req.params
+        const deleteUser = deleteUsersService({ id });
+        if (!deleteUser) {
+            res.status(404).json({ error: 'Usuario no encontrado' });
+        } else {
+            res.status(200).json({ msg: 'Usuario eliminado con éxito' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: error.message });
+    }
+};
+
+const updateUsers = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateUser = await updateUsersService( id, req.body );
+        res.status(200).json({ updateUser });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: error.message });
     }
 };
 
 module.exports = {
     createUsers,
     getAllUsers,
-    loginService
+    loginService,
+    deleteUsers,
+    updateUsers
 }
--- services/user.service.js
+++ services/user.service.js
@@ -2,29 +2,63 @@ const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+
+const encryptPassword = async (password) => {
+    const SALT_ROUNDS = 10;
+    const hashedPassword = await bcrypt.hash( password, SALT_ROUNDS );
+    return hashedPassword;
+}
+
+
+const validateName = (name) => {
+    const nameRegex = /^[a-zA-Z\s']+$/;
+    return nameRegex.test(name);
+}
+
+
+const validatePassword = (password) => {
+    // La expresión regular requiere al menos una mayúscula, una minúscula, un número y un carácter especial
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+};
+
+
 const createUser = async ({ name, lastName, userName, password, email, admin, active }) => {
 
-    const saltRounds = 10;
+    const passwordHashed = await encryptPassword(password);    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    const hashedPassword = await bcrypt.hash( password, saltRounds );
+    if (!validateName(name) || !validateName(lastName)) {
+        throw new Error('Nombre o apellido no válido');
+    }
+
+    if(!validatePassword(password)){
+        throw new Error('El formato de la contraseña no es valido');
+    }
+
+    if (!emailRegex.test(email)) {
+        throw new Error('El formato del correo electrónico no es válido');
+    }
 
     const user = new User({
         name,
         lastName,
         userName,
-        password: hashedPassword,
-        email, 
+        password: passwordHashed,
+        email: emailValidation, 
         admin,
         active
     });
 
     await user.save();
 
-    if(!user) throw new Error('No se a podido crear un nuevo usuario')
+    if(!user) throw new Error('Error al crear un nuevo usuario')
     return user;
 };
 
+
 const getAllUser = async ({ id, name }) => {
+
     let query = {};
 
     if(id){
@@ -37,9 +71,10 @@ const getAllUser = async ({ id, name }) => {
 
     const users = await User.find(query);
 
-    if(!users) throw new Error('No se pueden traer todos los usuarios')
+    if(!users) throw new Error('Error al traer todos los usuarios')
     return users
 };
+
 
 const login = async ({ userName, email, password }) => {
     const SECRET = process.env.SECRET_KEY;
@@ -72,10 +107,55 @@ const login = async ({ userName, email, password }) => {
 };
 
 
+const deleteUsersService = async ({ id }) => {
+
+    const deleteUser = await User.findByIdAndDelete(id);
+    if(!deleteUser) throw new Error('No se pudo eliminar el usuario')
+    return deleteUser
+
+};
+
+
+const updateUsersService = async ({ id, ...updates }) => {
+
+    const validFields = ['name', 'lastName', 'userName', 'password']; 
+
+    if (('name' in updates && !validateName(updates.name)) || ('lastName' in updates && !validateName(updates.lastName))) {
+        throw new Error('Nombre o apellido no válido');
+    }
+
+    if('password' in updates && !validatePassword(updates.password)){
+        throw new Error('La contraseña no es valida');
+    }
+
+    //some: verifica si al menos un valor cumple
+    //Object.keys: devuelve un array de las keys
+    if (!validFields.some(field => Object.keys(updates).includes(field))) {
+        throw new Error('Se requiere al menos un campo válido para la actualización');
+    }
+
+    // Obeject.entries() : crea un array de subarrays
+    // .filter(([key]) => validFields.includes(key)): Utiliza el método filter para crear un nuevo array que solo incluye los subarrays donde la clave (key) está presente en validFields. 
+    const filteredUpdates = Object.fromEntries(Object.entries(updates).filter(([key]) => validFields.includes(key)));
+
+    if (filteredUpdates.password) {
+        filteredUpdates.password = await encryptPassword(filteredUpdates.password);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(id, filteredUpdates, { new: true });
+
+    if (!updatedUser) {
+        throw new Error('Error al actualizar usuario');
+    }
+
+    return updatedUser;
+};
 
 
 module.exports = {
     createUser,
     getAllUser,
-    login
+    login,
+    deleteUsersService,
+    updateUsersService
 }
